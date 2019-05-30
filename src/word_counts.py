a0a68dbd064aa79002b7b96096b784a3b3ad4c2d
Chase Hardin <HardinDonaldC@JohnDeere.com>
2019-05-30 16:10:33 -0500

    Add execution to file.

--- src/word_counts.py
+++ src/word_counts.py
@@ -1,9 +1,22 @@
-from operator import add
-
-
-def do_word_counts(lines):
-    counts = (lines.flatMap(lambda x: x.split())
-              .map(lambda x: (x, 1))
-              .reduceByKey(add))
-
-    return {word: count for word, count in counts.collect()}
+# import sys
+# from operator import add
+#
+# from pyspark import SparkContext
+#
+#
+# def do_word_counts(lines):
+#     counts = (lines.flatMap(lambda x: x.split())
+#               .map(lambda x: (x, 1))
+#               .reduceByKey(add))
+#
+#     return {word: count for word, count in counts.collect()}
+#
+#
+# if __name__ == "__main__":
+#     if len(sys.argv) != 2:
+#         sys.exit("Usage: wordcount file}")
+#
+#     sc = SparkContext(appName="PythonWordCount")
+#     lines = sc.textFile(sys.argv[1], 1)
+#
+#     print(do_word_counts(lines))
